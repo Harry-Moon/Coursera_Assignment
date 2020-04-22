0d92a3273a01a37324738cb7a17d7748558c9ed0
Harry-Moon <62257069+Harry-Moon@users.noreply.github.com>
2020-04-22 14:24:33 +0200

    Update script.js

--- Module4_solution/script.js
+++ Module4_solution/script.js
@@ -1,4 +1,4 @@
-(function () {
+(function (window) {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
   var names = names [i];
@@ -12,6 +12,6 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 
-})();
+})(window);
 
 console.log(helloSpeaker);
