629958051e11b57c874279ce28ad1fa5e635c35f
Harry-Moon <62257069+Harry-Moon@users.noreply.github.com>
2020-04-22 14:12:02 +0200

    Update script.js

--- Module4_solution/script.js
+++ Module4_solution/script.js
@@ -1,7 +1,9 @@
 (function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
+  var names = names [i];
   var firstLetter = names[i].charAt(0).toLowerCase();
+  
 
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
